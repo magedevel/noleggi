88562a2d185ee60ac3b7f6d49496e27b91a6c408
3innovaweb@gmail.com <magedevel>
2018-05-16 06:06:37 +0000

    File generated by Skaffolder - user: 3innovaweb@gmail.com

---
+++ api/properties.php
@@ -0,0 +1,10 @@
+<?php 
+	$db_Noleggi_db_Url = 'localhost';
+	$db_Noleggi_db_User = 'root';
+	$db_Noleggi_db_Pass = '';
+	$db_Noleggi_db_DbName = 'noleggi_db';
+	
+	
+	$jwt_secret_key = 'mySecretKey';
+	
+?>
